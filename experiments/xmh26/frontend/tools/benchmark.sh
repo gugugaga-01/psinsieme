ed5d5170ec9ae64fdd129c7dcd390cadffb31ed0
gugugaga-03 <gugugaga3@johnsoncc.top>
2026-03-21 23:12:10 +0800

    feat:添加新功能代码

---
+++ experiments/xmh26/frontend/tools/benchmark.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Auto-generated script for n = 5, m = 13
+
+./bin/frontend.exe -n 5 -m 13 -p 0 &
+./bin/frontend.exe -n 5 -m 13 -p 1 &
+./bin/frontend.exe -n 5 -m 13 -p 2 &
+./bin/frontend.exe -n 5 -m 13 -p 3 &
+./bin/frontend.exe -n 5 -m 13 -p 4 &
